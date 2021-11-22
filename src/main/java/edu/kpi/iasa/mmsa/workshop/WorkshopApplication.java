2f5c02af62cd23072e3f432b03f43c69c2b72954
Iryna Afanasieva <iryna.v.afanasieva@gmail.com>
2021-11-22 09:17:15 +0200

    Add JWT

--- src/main/java/edu/kpi/iasa/mmsa/workshop/WorkshopApplication.java
+++ src/main/java/edu/kpi/iasa/mmsa/workshop/WorkshopApplication.java
@@ -1,13 +1,40 @@
 package edu.kpi.iasa.mmsa.workshop;
 
+import edu.kpi.iasa.mmsa.workshop.configuration.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class WorkshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopApplication.class, args);
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+					.and()
+					.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/login").permitAll()
+					.anyRequest().authenticated();
+		}
 	}
 
 }
---
+++ src/main/java/edu/kpi/iasa/mmsa/workshop/dto/JwtResponseDto.java
@@ -0,0 +1,14 @@
+package edu.kpi.iasa.mmsa.workshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponseDto {
+    String token;
+}
